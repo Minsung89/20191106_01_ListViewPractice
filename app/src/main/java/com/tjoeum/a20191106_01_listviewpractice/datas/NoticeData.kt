b9045036fc5c5cc8da95db4fdb0a8d2bd0de462a
Minsung89 <minsung0217@gmail.com>
2019-11-06 19:56:11 +0900

    데이터클래스 생성

---
+++ app/src/main/java/com/tjoeum/a20191106_01_listviewpractice/datas/NoticeData.kt
@@ -0,0 +1,10 @@
+package com.tjoeum.a20191106_01_listviewpractice.datas
+
+class NoticeData(title:String, content:String, writeDate:String) {
+
+    var title = title
+    var content = content
+    var date = writeDate
+
+
+}
